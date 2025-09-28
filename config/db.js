441a3c36021af183c3cc22f40ccff705602b03f9
RaviTeja-Bhima <rteja44628@gmail.com>
2025-09-28 23:13:22 +0530

    ✅ Setup db.js and sever.js

--- config/db.js
+++ config/db.js
@@ -1,9 +1,13 @@
+// src/db.js
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connected");
+    console.log("✅ MongoDB Atlas connected");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err);
     process.exit(1);
